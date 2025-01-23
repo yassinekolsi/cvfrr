15e146ee34dba739319ef732d1e87e33d3563765
yassinekolsi <yassine.kolsi@gmail.com>
2025-01-23 23:42:53 +0100

    Delete revvjava/svr directory

--- revvjava/svr/src/A.java
+++
@@ -1,6 +0,0 @@
-public class A {
-    int s;
-    public static String  toStringe() {
-        return "A [s="  + "]";
-    }
-}
--- revvjava/svr/src/B.java
+++
@@ -1,5 +0,0 @@
-public class B extends A {
-    public String toString(){
-        return "je suis cvn";
-    }
-}
